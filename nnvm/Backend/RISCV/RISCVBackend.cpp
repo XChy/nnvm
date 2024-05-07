61f200f2228966993a43069e863c5af46bec0ad4
XChy <xxs_chy@outlook.com>
2024-05-07 14:03:59 +0800

    [Backend] Remove some dead code

--- nnvm/Backend/RISCV/RISCVBackend.cpp
+++ nnvm/Backend/RISCV/RISCVBackend.cpp
@@ -36,36 +36,6 @@ void RISCVBackend::legalize(LowModule &module) {
       for (auto it = bb->insts.begin(); it != bb->insts.end(); it++) {
         uint64_t type = (uint64_t)it->type;
         if (it->type <= LowInst::ISA_BEGIN) {
-          switch ((InstID)type) {
-          case InstID::Load:
-            if (it->operand[0].bitwidth() == 32) {
-              it->type = LowInst::LW;
-              it->operand[2] = it->operand[1];
-              it->operand[1] = LowOperand::imm(0);
-            }
-            break;
-          case InstID::Store:
-            if (it->operand[0].bitwidth() == 32) {
-              it->type = LowInst::SW;
-              it->operand[2] = it->operand[1];
-              it->operand[1] = LowOperand::imm(0);
-            }
-            break;
-          case InstID::Stack: {
-            LowOperand op = it->operand[0];
-            LowOperand size = it->operand[1];
-            // TODO: Too big stack?
-            bb->insts.insert(
-                it, LowInst{.type = LowInst::ADDI,
-                            .operand = {getDef(getRegForStack()),
-                                        getUse(getRegForStack()),
-                                        LowOperand::imm(-size.immValue)}});
-            *it = LowInst{LowInst::MV, {getDef(op), getUse(getRegForStack())}};
-            break;
-          }
-          default:
-            break;
-          }
         }
       }
     }
