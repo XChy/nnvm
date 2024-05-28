3402a225e709832fbeb445f8d4e686cb74b02208
XChy <xxs_chy@outlook.com>
2024-05-29 01:19:52 +0800

    Test action

--- educg/sync_repo.sh
+++ educg/sync_repo.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+set -e
+
 cd nnvm_submit
-git checkout main
+git checkout master
 cd ..
 rm -rf nnvm_submit/src
 
-cp -r nnvm nnvm_submit/src/
+mkdir -p nnvm_submit/src/nnvm
+cp -r nnvm nnvm_submit/src/nnvm
 cp -r antlr-runtime nnvm_submit/src/
 
 date > nnvm_submit/timestamp
 cd nnvm_submit
 git add .
 git commit -m "sync $(date)"
-git push -u origin main
+git push -u origin master
---
+++ runner/compile_and_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+riscvgcc="riscv64-unknown-linux-gnu-gcc"
+
+root_dir=$(dirname $(dirname $0))
+compiler=$root_dir/build/compiler
+
+asm=$(mktemp --suffix=".s")
+obj=$(mktemp --suffix=".o")
+sylib=$root_dir/build/libsylib.a
+mainexec=$(mktemp --suffix=".out")
+
+
+$compiler $1 -o $asm
+$riscvgcc -c $asm -o $obj
+$riscvgcc $obj $sylib -o $mainexec
+
+qemu-riscv64 -L "/usr/riscv64-linux-gnu" $mainexec console=ttyS0
