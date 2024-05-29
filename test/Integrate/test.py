07ffe402c4a3f490531653e646663f7d9fe69201
Fradow <cirnofantasy@gmail.com>
2024-05-29 18:18:39 +0800

    [Test] Enable frontend test

--- test/Integrate/test.py
+++ test/Integrate/test.py
@@ -1,4 +1,14 @@
 #!python3
+"""Usage: python3 test.py [OPTION]... [FILE]...
+Test all FILEs, or all the .sy files under $TEST_DIR if no FILE is specified.
+
+Options:
+  -b, --brief             Show failed cases only
+  -d, --difftest          Differential test mode
+  -e, --regexp=PATTERN    Test files whose names contain PATTERN, with FILEs ignored. Multiple PATTERNs are allowed
+  -f, --frontend          Frontend test mode
+  -h, --help              Display help text and exit
+"""
 
 import subprocess
 import tempfile
@@ -7,153 +17,187 @@ import os.path as path
 import re
 import sys
 import getopt
-from threading import Timer
-
-test_dir = path.split(path.abspath(__file__))[0]
-root_dir = path.split(path.split(test_dir)[0])[0]
-compiler = path.join(root_dir, "build", "compiler")
-sylib = path.join(root_dir, "build", "libsylib.a")
-
-asm = tempfile.mktemp(suffix=".s")
-obj = tempfile.mktemp(suffix=".o")
-mainexec = tempfile.mktemp(suffix=".out")
-
-riscvgcc = "riscv64-linux-gnu-gcc"
-
-frontend_mode = False
-difftest_mode = False
 
 
-def get_expected(source):
-  out_path = path.splitext(source)[0] + ".out"
+TEST_DIR = path.split(path.abspath(__file__))[0]
+ROOT_DIR = path.split(path.split(TEST_DIR)[0])[0]
+NNVM = path.join(ROOT_DIR, 'build', 'compiler')
+SYLIB_RV = path.join(ROOT_DIR, 'build', 'libsylib.a')
+SYLIB_X86 = path.join(ROOT_DIR, 'build', 'x86-64libsy.o')
+SYLIB_SRC = path.join(ROOT_DIR, 'test', 'Runtime', 'sylib.c')
+
+GCC_RV = 'riscv64-linux-gnu-gcc'
+GCC_X86 = 'gcc'
+LLC = 'llc'
+QEMU = 'qemu-riscv64'
+
+TIMEOUT_PERIOD = 2
+
+
+def __get_expected(src: str):
+  out_path = path.splitext(src)[0] + ".out"
   if path.exists(out_path):
     with open(out_path) as f:
-      return "".join(f.readlines())
+      return ''.join(f.readlines())
 
-  with open(source) as f:
+  with open(src) as f:
     line = f.readlines()[1]
     searched_groups = re.search('EXPECTED:(.*)', line)
     return ("" if searched_groups is None else searched_groups.group(1))
 
 
-def get_input(source):
-  in_path = path.splitext(source)[0] + ".in"
+def __get_input(src: str):
+  in_path = path.splitext(src)[0] + ".in"
   if path.exists(in_path):
     with open(in_path) as f:
-      return "".join(f.readlines())
+      return ''.join(f.readlines())
 
-  with open(source) as f:
+  with open(src) as f:
     line = f.readlines()[0]
     searched_groups = re.search('INPUT:(.*)', line)
     return ("" if searched_groups is None else searched_groups.group(1))
 
 
-def test(source, reported_name):
-  ret = subprocess.Popen(
-      [compiler, source, "-o", asm], stdout=subprocess.PIPE, encoding="UTF-8", stderr=subprocess.DEVNULL)
-  stdout, stderr = ret.communicate()
+def __print_error(msg: str, rel_path: str):
+  print(f'\033[31m{msg}\033[0m on {rel_path}')
+
+
+tmp_asm = tempfile.NamedTemporaryFile(suffix='.s', delete=False)
+tmp_obj = tempfile.NamedTemporaryFile(suffix='.o', delete=False)
+tmp_out = tempfile.NamedTemporaryFile(suffix='.out', delete=False)
+
+frontend_mode = False
+difftest_mode = False
+
+
+def test(src: str, rel_path: str):
+  SUBPROC_ARGLISTS_RV = [
+      [NNVM, src, '-o', tmp_asm.name],
+      [GCC_RV, '-c', tmp_asm.name, '-o', tmp_obj.name],
+      [GCC_RV, tmp_obj.name, SYLIB_RV, '-o', tmp_out.name],
+      [QEMU, '-L', '/usr/riscv64-linux-gnu', tmp_out.name, 'console=ttyS0'],
+  ]
+  SUBPROC_ARGLISTS_FRONTEND = [
+      [NNVM, src, '--backend', 'llvm', '-o', tmp_asm.name],
+      [LLC, '-O2', '--filetype=obj', tmp_asm.name, '-o', tmp_obj.name],
+      [GCC_X86, tmp_obj.name, SYLIB_X86, '-o', tmp_out.name],
+      [tmp_out.name, 'console=ttyS0']
+  ]
+  subproc_arglists = SUBPROC_ARGLISTS_FRONTEND if frontend_mode else SUBPROC_ARGLISTS_RV
+
+  # source code -> assembly code
+  ret = subprocess.run(subproc_arglists[0], stdout=subprocess.PIPE,
+                       stderr=subprocess.DEVNULL, encoding='UTF-8')
+  tmp_asm.close()
   if ret.returncode != 0:
-    print("COMPLIATION ERROR on", reported_name, "")
+    __print_error('COMPLIATION FAILED', rel_path)
     return False
-  else:
-    # Use assembler to generate binary.
-    ret = subprocess.run(
-        [riscvgcc, "-c", asm, "-o", obj])
-    if ret.returncode != 0:
-      print("ASSEMBLER FAILED", reported_name, "")
-      return False
-    ret = subprocess.run(
-        [riscvgcc, obj, sylib, "-o", mainexec], stderr=subprocess.DEVNULL)
-    if ret.returncode != 0:
-      print("GCC FAILED", reported_name, "")
-      return False
 
-    # parse input and output
-    inputed = get_input(source).strip() + "\n"
-    expected = get_expected(source).strip()
+  # assembly code -> objects
+  ret = subprocess.run(subproc_arglists[1], stdout=subprocess.DEVNULL,
+                       stderr=subprocess.DEVNULL, encoding='UTF-8')
+  tmp_obj.close()
+  if ret.returncode != 0:
+    __print_error('ASSEMBLING FAILED', rel_path)
+    return False
 
-    ret = subprocess.Popen(["qemu-riscv64", "-L", "/usr/riscv64-linux-gnu", mainexec, "console=ttyS0"],
-                           stdout=subprocess.PIPE, stdin=subprocess.PIPE,
-                           encoding="UTF-8")
-    timer = Timer(2, ret.kill)
-    try:
-      timer.start()
-      actual, stderr = ret.communicate(input=inputed)
-      if source.count("functional"):
-        actual = (actual + ("" if actual.endswith('\n')
-                  else "\n") + str(ret.wait())).strip()
-    finally:
-      if not timer.is_alive():
-        print("TIMEOUT on", reported_name)
-        timer.cancel()
-        return False
-      timer.cancel()
+  # objects -> executable
+  ret = subprocess.run(subproc_arglists[2], stdout=subprocess.DEVNULL,
+                       stderr=subprocess.DEVNULL, encoding='UTF-8')
+  tmp_out.close()
+  if ret.returncode != 0:
+    __print_error('LINKAGE FAILED', rel_path)
+    return False
 
-    if actual != expected:
-      print("FAILED on", reported_name, ", actual",
-            actual, ", expected", expected)
-      return False
+  # run the executable
+  input_text = __get_input(src).strip()
+  expected_text = __get_expected(src).strip()
+  try:
+    ret = subprocess.run(subproc_arglists[3],
+                         input=input_text, capture_output=True, text=True,
+                         encoding='UTF-8', timeout=TIMEOUT_PERIOD)
+    if len(ret.stdout) == 0:
+      actual_text = str(ret.returncode)
     else:
-      print("PASSED", reported_name)
-      return True
+      actual_text = f'{ret.stdout.strip()}\n{ret.returncode}'
+  except subprocess.TimeoutExpired:
+    __print_error('TIMEOUT on', rel_path)
+    return False
+
+  if actual_text != expected_text:
+    print(
+        f'FAILED on {rel_path}.\n\033[31mACTUAL\033[0m:\n{actual_text}\n\033[35mEXPECTED\033[0m:\n{expected_text}')
+    return False
+  print(f'\033[32mPASSED\033[0m {rel_path}')
+  return True
 
 
 def __add_test(test_set: set, src: str):
   test_set.add(path.expanduser(src))
 
 
-def __walk_test_dir(test_set: set, pattern: str):
-  for root, _, filenames in os.walk(test_dir):
+def __walk_test_dir(test_set: set, patterns: list):
+  for root, _, filenames in os.walk(TEST_DIR):
     for filename in filenames:
       src = path.join(root, filename)
-      if (not src.endswith('.sy') or
-              len(pattern) != 0 and not re.search(pattern, src)):
+      if (not src.endswith('.sy') or len(patterns) != 0
+              and not any(re.search(pattern, src) for pattern in patterns)):
         continue
       __add_test(test_set, src)
 
 
-def main():
-  """ Usage: python3 test.py [OPTION]... [FILE]...
-  Test all FILEs, or all the tests under $TEST_DIR if no FILE is specified.
+def __init_difftest_mode():
+  global difftest_mode
+  difftest_mode = True
 
-  Options:
-    -e, --regexp=PATTERNS   test files whose name match PATTERNS, ONLY if no FILE is specified
-    -f, --frontend          frontend-testing mode
-    -h, --help              display help text and exit
-  """
+
+def __init_frontend_mode():
+  global frontend_mode
+  frontend_mode = True
+  # compile sylib(x86-64) for frontend test
+  try:
+    subprocess.run(
+        [GCC_X86, "-c", SYLIB_SRC, "-o", SYLIB_X86], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, encoding="UTF-8", check=True)
+  except subprocess.CalledProcessError as err:
+    print(err)
+    exit(1)
+
+
+def main():
   try:
     opts, args = getopt.getopt(sys.argv[1:], 'de:fh', [
                                'difftest', 'regexp', 'frontend', 'help'])
   except getopt.GetoptError as err:
     print(err)
     exit(1)
-  pattern = ''
+  patterns = []
   for opt, arg in opts:
     if opt in ['-h', '--help']:
-      print(main.__doc__)
+      print(__doc__)
       exit(0)
     elif opt in ['-d', '--difftest']:
-      difftest_mode = True
+      __init_difftest_mode()
     elif opt in ['-e', '--regexp']:
-      pattern = arg
+      patterns.append(arg)
     elif opt in ['-f', '--frontend']:
-      frontend_mode = True
+      __init_frontend_mode()
+
   test_set = set()
   for file in args:
     __add_test(test_set, file)
   if len(test_set) == 0:
-    __walk_test_dir(test_set, pattern)
+    __walk_test_dir(test_set, patterns)
 
   passed_cnt, total_cnt = 0, 0
   for src in test_set:
-    rel_path = path.relpath(src, test_dir)
+    rel_path = path.relpath(src, TEST_DIR)
     print(f"[{total_cnt}] Running on", rel_path)
     passed = test(src, rel_path)
     if passed:
       passed_cnt += 1
     total_cnt += 1
 
-  print("============Complete Test=============")
+  print("============ Test Complete =============")
   print(f"Total: {total_cnt}")
   print(f"Passed: {passed_cnt}")
   print(f"Failed: {total_cnt - passed_cnt}")
