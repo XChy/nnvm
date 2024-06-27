4d5966939bb5400c1ca644c08383fe4e5da73d93
XChy <xxs_chy@outlook.com>
2024-06-27 16:27:26 +0800

    [Educg] Add placeholder

--- antlr-runtime/atn/HashUtils.h
+++ antlr-runtime/atn/HashUtils.h
@@ -1,20 +1,18 @@
-﻿/* Copyright (c) 2012-2017 The ANTLR Project. All rights reserved.
+/* Copyright (c) 2022 The ANTLR Project. All rights reserved.
  * Use of this file is governed by the BSD 3-clause license that
  * can be found in the LICENSE.txt file in the project root.
  */
 
 #pragma once
 
-#include "antlr4-common.h"
+#include <cstddef>
 
 namespace antlr4 {
 namespace atn {
 
-  /// Represents the type of recognizer an ATN applies to.
-  enum class ATNType {
-    LEXER = 0,
-    PARSER = 1,
-  };
+  inline bool cachedHashCodeEqual(size_t lhs, size_t rhs) {
+    return lhs == rhs || lhs == 0 || rhs == 0;
+  }
 
 } // namespace atn
 } // namespace antlr4
--- educg/sync_repo.sh
+++ educg/sync_repo.sh
@@ -9,7 +9,7 @@ rm -rf nnvm_submit/src
 
 mkdir -p nnvm_submit/src/nnvm
 cp -r nnvm nnvm_submit/src/nnvm
-cp -r antlr-runtime nnvm_submit/src/
+touch nnvm_submit/src/nnvm/nnvm/placeholder.h
 
 date > nnvm_submit/timestamp
 cd nnvm_submit
