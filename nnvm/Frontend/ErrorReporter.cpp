2e493c8299cb4f4bc78e2d34de07a61272e56fe4
myywee <myweethon@outlook.com>
2024-08-05 20:26:47 +0800

    [Frontend]:fix ErrorReporter

--- nnvm/Frontend/ErrorReporter.cpp
+++ nnvm/Frontend/ErrorReporter.cpp
@@ -8,31 +8,6 @@ using antlr4::Token;
 
 ErrorReporter::ErrorReporter() {}
 
-void ErrorReporter::errorBtype(SysYParser::BtypeContext *ctx, const std::string &msg) {
-    Token *temp = ctx->getStart();
-    size_t line = temp->getLine();
-    size_t charPositonInLine = temp->getCharPositionInLine();
-    std::string error = "line" + std::to_string(line) + ":" + std::to_string(charPositonInLine) + ": " + msg;
-    errors.push_back(error);
-}
-
-
-void ErrorReporter::errorFuncType(SysYParser::FuncTypeContext *ctx, const std::string &msg) {
-    Token *temp = ctx->getStart();
-    size_t line = temp->getLine();
-    size_t charPositonInLine = temp->getCharPositionInLine();
-    std::string error = "line" + std::to_string(line) + ":" + std::to_string(charPositonInLine) + ": " + msg;
-    errors.push_back(error);
-}
-
-void ErrorReporter::errorConstInitVal(SysYParser::ConstInitValContext *ctx, const std::string &msg) {
-    Token *temp = ctx->getStart();
-    size_t line = temp->getLine();
-    size_t charPositonInLine = temp->getCharPositionInLine();
-    std::string error = "line" + std::to_string(line) + ":" + std::to_string(charPositonInLine) + ": " + msg;
-    errors.push_back(error);
-}
-
 void ErrorReporter::errorRecord(antlr4::ParserRuleContext *ctx, const std::string &msg) {
     Token *temp = ctx->getStart();
     size_t line = temp->getLine();
--- nnvm/Frontend/ErrorReporter.h
+++ nnvm/Frontend/ErrorReporter.h
@@ -10,12 +10,6 @@ class ErrorReporter{
     public :
         std::vector<std::string> errors;
 
-        void errorBtype(SysYParser::BtypeContext *ctx, const std::string &msg);
-
-        void errorFuncType(SysYParser::FuncTypeContext * ctx, const std::string &msg);
-    
-        void errorConstInitVal(SysYParser::ConstInitValContext *ctx, const std::string &msg);
-
         void errorRecord(antlr4::ParserRuleContext * ctx, const std::string &msg);
 
         void report();
