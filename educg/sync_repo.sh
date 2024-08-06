e618bb1c4699857967da75af129be262a5495c50
XChy <xxs_chy@outlook.com>
2024-08-07 00:07:36 +0800

    [educg] Update script to upload license and readme

--- educg/sync_repo.sh
+++ educg/sync_repo.sh
@@ -10,6 +10,8 @@ rm -rf nnvm_submit/src
 mkdir -p nnvm_submit/src/nnvm
 cp -r nnvm nnvm_submit/src/nnvm
 touch nnvm_submit/src/nnvm/nnvm/placeholder.h
+cp LICENSE nnvm_submit/LICENSE
+cp README.md nnvm_submit/README.md
 
 date > nnvm_submit/timestamp
 cd nnvm_submit
