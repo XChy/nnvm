b0e6daa68db4b9fabbfcb8e861dbb5ded6885843
XChy <xxs_chy@outlook.com>
2024-08-18 23:31:54 +0800

    [Pass] Disable pass output

--- test/Runtime/csmith.h
+++ test/Runtime/csmith.h
@@ -1,7 +1,9 @@
 /*
- * Reference: https://github.com/dtcxzyw/cmmc/blob/main/tests/csmith/csmith_header.h
+ * Reference:
+ * https://github.com/dtcxzyw/cmmc/blob/main/tests/csmith/csmith_header.h
  * Provide safe calculations for the random test cases generated my csmith.
- * All the ternary operators in original version were substituted with if-else statements for the compatibility of SysY grammar.
+ * All the ternary operators in original version were substituted with if-else
+ * statements for the compatibility of SysY grammar.
  */
 
 static uint32_t crc32_tab[256];
@@ -17,8 +19,7 @@ static void crc32_gentab(void) {
     for (j = 8; j > 0; j--) {
       if (crc & 1) {
         crc = (crc >> 1) ^ poly;
-      }
-      else {
+      } else {
         crc >>= 1;
       }
     }
@@ -35,7 +36,7 @@ static float fabsf(float x) {
 
 static void crc32_byte(uint8_t b) {
   crc32_context = ((crc32_context >> 8) & 0x00FFFFFF) ^
-    crc32_tab[(crc32_context ^ b) & 0xFF];
+                  crc32_tab[(crc32_context ^ b) & 0xFF];
 }
 
 static void crc32_4bytes(uint64_t val) {
@@ -106,24 +107,24 @@ static int32_t safe_unary_minus_func_int32_t_s(int32_t si) {
 }
 static int32_t safe_add_func_int32_t_s_s(int32_t si1, int32_t si2) {
   if ((((si1 > 0) && (si2 > 0) && (si1 > ((2147483647) - si2))) ||
-    ((si1 < 0) && (si2 < 0) && (si1 < ((-2147483647 - 1) - si2))))) {
+       ((si1 < 0) && (si2 < 0) && (si1 < ((-2147483647 - 1) - si2))))) {
     return ((si1));
   }
   return (si1 + si2);
 }
 static int32_t safe_sub_func_int32_t_s_s(int32_t si1, int32_t si2) {
   if ((((si1 ^ si2) & (((si1 ^ ((si1 ^ si2) & (~(2147483647)))) - si2) ^ si2)) <
-    0)) {
+       0)) {
     return ((si1));
   }
   return (si1 - si2);
 }
 static int32_t safe_mul_func_int32_t_s_s(int32_t si1, int32_t si2) {
   if ((((si1 > 0) && (si2 > 0) && (si1 > ((2147483647) / si2))) ||
-    ((si1 > 0) && (si2 <= 0) && (si2 < ((-2147483647 - 1) / si1))) ||
-    ((si1 <= 0) && (si2 > 0) && (si1 < ((-2147483647 - 1) / si2))) ||
-    ((si1 <= 0) && (si2 <= 0) && (si1 != 0) &&
-      (si2 < ((2147483647) / si1))))) {
+       ((si1 > 0) && (si2 <= 0) && (si2 < ((-2147483647 - 1) / si1))) ||
+       ((si1 <= 0) && (si2 > 0) && (si1 < ((-2147483647 - 1) / si2))) ||
+       ((si1 <= 0) && (si2 <= 0) && (si1 != 0) &&
+        (si2 < ((2147483647) / si1))))) {
     return ((si1));
   }
   return si1 * si2;
@@ -142,13 +143,14 @@ static int32_t safe_div_func_int32_t_s_s(int32_t si1, int32_t si2) {
 }
 static int32_t safe_lshift_func_int32_t_s_s(int32_t left, int right) {
   if (((left < 0) || ((right) < 0) || ((right) >= 32) ||
-    (left > ((2147483647) >> (right))))) {
+       (left > ((2147483647) >> (right))))) {
     return ((left));
   }
   return (left << (right));
 }
 static int32_t safe_lshift_func_int32_t_s_u(int32_t left, uint32_t right) {
-  if (((left < 0) || ((right) >= 32) || (left > ((2147483647) >> (right))))) {
+  if (((left < 0) || ((right) < 0) || ((right) >= 32) ||
+       (left > ((2147483647) >> (right))))) {
     return ((left));
   }
   return (left << (right));
@@ -160,58 +162,79 @@ static int32_t safe_rshift_func_int32_t_s_s(int32_t left, int right) {
   return (left >> (right));
 }
 static int32_t safe_rshift_func_int32_t_s_u(int32_t left, uint32_t right) {
-  if (((left < 0) || ((right) >= 32))) {
+  if (((left < 0) || ((right) < 0) || ((right) >= 32))) {
     return ((left));
   }
   return (left >> (right));
 }
 
-static uint32_t safe_unary_minus_func_uint32_t_u(uint32_t ui) { return -ui; }
-static uint32_t safe_add_func_uint32_t_u_u(uint32_t ui1, uint32_t ui2) {
-
-  return ui1 + ui2;
-}
-static uint32_t safe_sub_func_uint32_t_u_u(uint32_t ui1, uint32_t ui2) {
-
-  return ui1 - ui2;
-}
-static uint32_t safe_mul_func_uint32_t_u_u(uint32_t ui1, uint32_t ui2) {
-
-  return (ui1) * (ui2);
-}
-static uint32_t safe_mod_func_uint32_t_u_u(uint32_t ui1, uint32_t ui2) {
-  if ((ui2 == 0)) {
-    return ((ui1));
+static uint32_t safe_unary_minus_func_uint32_t_u(uint32_t si) {
+  if ((si == (-2147483647 - 1))) {
+    return ((si));
   }
-  return (ui1 % ui2);
+  return -si;
 }
-static uint32_t safe_div_func_uint32_t_u_u(uint32_t ui1, uint32_t ui2) {
-  if ((ui2 == 0)) {
-    return ((ui1));
+
+static uint32_t safe_add_func_uint32_t_u_u(uint32_t si1, uint32_t si2) {
+  if ((((si1 > 0) && (si2 > 0) && (si1 > ((2147483647) - si2))) ||
+       ((si1 < 0) && (si2 < 0) && (si1 < ((-2147483647 - 1) - si2))))) {
+    return ((si1));
   }
-  return (ui1 / ui2);
+  return (si1 + si2);
+}
+static uint32_t safe_sub_func_uint32_t_u_u(uint32_t si1, uint32_t si2) {
+
+  if ((((si1 ^ si2) & (((si1 ^ ((si1 ^ si2) & (~(2147483647)))) - si2) ^ si2)) <
+       0)) {
+    return ((si1));
+  }
+  return (si1 - si2);
+}
+static uint32_t safe_mul_func_uint32_t_u_u(uint32_t si1, uint32_t si2) {
+  if ((((si1 > 0) && (si2 > 0) && (si1 > ((2147483647) / si2))) ||
+       ((si1 > 0) && (si2 <= 0) && (si2 < ((-2147483647 - 1) / si1))) ||
+       ((si1 <= 0) && (si2 > 0) && (si1 < ((-2147483647 - 1) / si2))) ||
+       ((si1 <= 0) && (si2 <= 0) && (si1 != 0) &&
+        (si2 < ((2147483647) / si1))))) {
+    return ((si1));
+  }
+  return si1 * si2;
+}
+static uint32_t safe_mod_func_uint32_t_u_u(uint32_t si1, uint32_t si2) {
+  if (((si2 == 0) || ((si1 == (-2147483647 - 1)) && (si2 == (-1))))) {
+    return ((si1));
+  }
+  return (si1 % si2);
+}
+
+static uint32_t safe_div_func_uint32_t_u_u(uint32_t si1, uint32_t si2) {
+  if (((si2 == 0) || ((si1 == (-2147483647 - 1)) && (si2 == (-1))))) {
+    return ((si1));
+  }
+  return (si1 / si2);
 }
 static uint32_t safe_lshift_func_uint32_t_u_s(uint32_t left, int right) {
-  if ((((right) < 0) || ((right) >= 32) ||
-    (left > ((4294967295U) >> (right))))) {
+  if (((left < 0) || ((right) < 0) || ((right) >= 32) ||
+       (left > ((2147483647) >> (right))))) {
     return ((left));
   }
   return (left << (right));
 }
 static uint32_t safe_lshift_func_uint32_t_u_u(uint32_t left, uint32_t right) {
-  if ((((right) >= 32) || (left > ((4294967295U) >> (right))))) {
+  if (((left < 0) || ((right) < 0) || ((right) >= 32) ||
+       (left > ((2147483647) >> (right))))) {
     return ((left));
   }
   return (left << (right));
 }
 static uint32_t safe_rshift_func_uint32_t_u_s(uint32_t left, int right) {
-  if ((((right) < 0) || ((right) >= 32))) {
+  if (((left < 0) || ((right) < 0) || ((right) >= 32))) {
     return ((left));
   }
   return (left >> (right));
 }
 static uint32_t safe_rshift_func_uint32_t_u_u(uint32_t left, uint32_t right) {
-  if (((right) >= 32)) {
+  if (((left < 0) || ((right) < 0) || ((right) >= 32))) {
     return ((left));
   }
   return (left >> (right));
@@ -219,32 +242,32 @@ static uint32_t safe_rshift_func_uint32_t_u_u(uint32_t left, uint32_t right) {
 
 static float safe_add_func_float_f_f(float sf1, float sf2) {
   if ((fabsf((0.5f * sf1) + (0.5f * sf2)) >
-    (0.5f * 3.40282346638528859811704183484516925e+38F))) {
+       (0.5f * 3.40282346638528859811704183484516925e+38F))) {
     return (sf1);
   }
   return (sf1 + sf2);
 }
 static float safe_sub_func_float_f_f(float sf1, float sf2) {
   if ((fabsf((0.5f * sf1) - (0.5f * sf2)) >
-    (0.5f * 3.40282346638528859811704183484516925e+38F))) {
+       (0.5f * 3.40282346638528859811704183484516925e+38F))) {
     return (sf1);
   }
   return (sf1 - sf2);
 }
 static float safe_mul_func_float_f_f(float sf1, float sf2) {
   if ((fabsf((0x1.0p-100f * sf1) * (0x1.0p-28f * sf2)) >
-    (0x1.0p-100f *
-      (0x1.0p-28f * 3.40282346638528859811704183484516925e+38F)))) {
+       (0x1.0p-100f *
+        (0x1.0p-28f * 3.40282346638528859811704183484516925e+38F)))) {
     return (sf1);
   }
   return (sf1 * sf2);
 }
 static float safe_div_func_float_f_f(float sf1, float sf2) {
   if (((fabsf(sf2) < 1.0f) &&
-    (((sf2 == 0.0f) ||
-      (fabsf((0x1.0p-49f * sf1) / (0x1.0p100f * sf2))) >
-      (0x1.0p-100f *
-        (0x1.0p-49f * 3.40282346638528859811704183484516925e+38F)))))) {
+       (((sf2 == 0.0f) ||
+         (fabsf((0x1.0p-49f * sf1) / (0x1.0p100f * sf2))) >
+             (0x1.0p-100f *
+              (0x1.0p-49f * 3.40282346638528859811704183484516925e+38F)))))) {
     return (sf1);
   }
   return (sf1 / sf2);
@@ -256,30 +279,3 @@ static int32_t safe_convert_func_float_to_int32_t(float sf1) {
   return ((sf1));
 }
 
-/*
-static double safe_add_func_double_f_f(double sf1, double sf2) {
-  if ((fabs((0.5 * sf1) + (0.5 * sf2)) > (0.5 *
-((double)1.79769313486231570814527423731704357e+308)))) { return (sf1);
-  }
-  return (sf1 + sf2);
-}
-static double safe_sub_func_double_f_f(double sf1, double sf2) {
-  if ((fabs((0.5 * sf1) - (0.5 * sf2)) > (0.5 *
-((double)1.79769313486231570814527423731704357e+308)))) { return (sf1);
-  }
-  return (sf1 - sf2);
-}
-static double safe_mul_func_double_f_f(double sf1, double sf2) {
-  if ((fabs((0x1.0p-100 * sf1) * (0x1.0p-924 * sf2)) > (0x1.0p-100 * (0x1.0p-924
-* ((double)1.79769313486231570814527423731704357e+308))))) { return (sf1);
-  }
-  return (sf1 * sf2);
-}
-static double safe_div_func_double_f_f(double sf1, double sf2) {
-  if (((fabs(sf2) < 1.0) && (((sf2 == 0.0) || (fabs((0x1.0p-974 * sf1) /
-(0x1.0p100 * sf2))) > (0x1.0p-100 * (0x1.0p-974 *
-((double)1.79769313486231570814527423731704357e+308))))))) { return (sf1);
-  }
-  return (sf1 / sf2);
-}
-*/
